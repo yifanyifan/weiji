ac4b706b9e5fe7b246980ddb32f0eed71f1ca642
yifan <895115828@qq.com>
2020-05-07 15:58:29 +0800

    yf

--- weiji-service/src/main/java/com/fujiang/weiji/controller/product/ProductController.java
+++ weiji-service/src/main/java/com/fujiang/weiji/controller/product/ProductController.java
@@ -2,7 +2,6 @@ package com.fujiang.weiji.controller.product;
 
 import com.fujiang.weiji.entity.product.Product;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +18,13 @@ public class ProductController {
     @Value("${from}")
     private String value;
 
+    @Value("${server.port}")
+    private String port;
+
     @RequestMapping(value = "/getProduct", method = RequestMethod.GET)
-    public String getProduct() {
+    public String getProduct() throws InterruptedException {
         //实现
         Product product = new Product();
-        return value + product.toString();
+        return port + ":" + value + product.toString();
     }
 }
