6f0fce6b938f0068b8a40332f98cbceb95b69904
yifan <895115828@qq.com>
2020-05-11 13:55:45 +0800

    yf

--- weiji-service/src/main/java/com/fujiang/weiji/controller/product/ProductController.java
+++ weiji-service/src/main/java/com/fujiang/weiji/controller/product/ProductController.java
@@ -25,7 +25,7 @@ public class ProductController {
     public String getProduct() throws InterruptedException {
         //实现
         Product product = new Product();
-        Thread.sleep(4000);
+        Thread.sleep(3000);
         return port + ":" + value + product.toString();
     }
 }
