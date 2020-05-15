c0ffaaadb620fc69daa52e7b751bcfefa1cd2c1d
yifan <895115828@qq.com>
2020-05-15 17:43:40 +0800

    yf

--- 21-package.sh
+++ 21-package.sh
@@ -2,10 +2,18 @@
 source ./20-functions.sh
 
 echo '=================================================================='
-echo 'step1: 打包eship-common, eship-export-data, eship-label-ex, customer'
-echo 'setp2: 压缩全部jar 到 /parcels/deploy/date/, 不包含三方lib。 默认将压缩包保存到windows D盘 parcels目录 或 linux的 /d/parcels/目录'
-echo '2019-12-9 by LJ'
+echo 'step1: 打包weiji-eureka weiji-config weiji-service weiji-customer'
+echo 'setp2: 压缩全部jar 到 /parcels/deploy/date/, 不包含三方lib。 默认将压缩包保存到windows D盘 weiji目录 或 linux的 /d/weiji/目录'
 echo '=================================================================='
 
+echo -e "\n\n============================================================更新代码, LJ@2019-12-13\n"
+read -p "是否pull, 更新代码?[y/n]?" isPull
+
+if [[ $isPull == 'y' ]];then
+
+    git reset --hard
+    git pull
+
+fi
 
 
