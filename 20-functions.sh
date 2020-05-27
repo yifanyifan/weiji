5f0034524178c8a920bab972279eb4508f697171
yifan <895115828@qq.com>
2020-05-27 17:07:25 +0800

    yf

--- 20-functions.sh
+++ 20-functions.sh
@@ -9,31 +9,20 @@ echo '=================================================================='
 function setRemoteDebugInTestStartup()
 {
     echo -e "\n\n===================================================== 为测试startup设置远程调试端口\n"
-	
     project=$1
-
     if [[ $project == 'weiji-service' ]];then
-
             #sed -i 's/-Dspring.profiles.active=prod/-Dspring.profiles.active=test -Djava.rmi.server.hostname=192.168.8.167 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=6022 #-Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=7022,suspend=n/g' `grep "java -jar" -rl #--include=test-startup.sh */`
-
     elif [[ $project == 'weiji-customer' ]];then
-
             #sed -i 's/-Dspring.profiles.active=prod/-Dspring.profiles.active=test -Djava.rmi.server.hostname=192.168.8.167 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=6028 #-Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=7028,suspend=n/g' `grep "java -jar" -rl #--include=test-startup.sh */`
-
     fi
-
 }
-
-
 
 # '=================================================================='
 # '获取本机局域网IP'
 # '=================================================================='
 function getIP()
 {
-
     local_ip=`ip addr |grep inet |grep 255 |grep -v 127.0.0.1|grep -v inet6 | awk '{print $2}' | awk -F "/" '{print $1}'`
-
     hostIP=""
     for element in $local_ip
     do
@@ -45,6 +34,5 @@ function getIP()
           break
         fi
     done
-
     echo $hostIP
 }
--- 21-package.sh
+++ 21-package.sh
@@ -6,20 +6,17 @@ echo 'step1: 打包weiji-eureka weiji-config weiji-service weiji-customer'
 echo 'setp2: 压缩全部jar 到 /parcels/deploy/date/, 不包含三方lib。 默认将压缩包保存到windows D盘 weiji目录 或 linux的 /d/weiji/目录'
 echo '=================================================================='
 
-echo -e "\n\n============================================================更新代码, LJ@2019-12-13\n"
+echo -e "\n\n============================================================更新代码\n"
 read -p "是否pull, 更新代码?[y/n]?" isPull
-
 if [[ $isPull == 'y' ]];then
-
     git reset --hard
     git pull
-
 fi
 
 
 
-
-echo -e "\n\n============================================================编译, LJ@2019-12-9\n"
+:<<MULTILINECOMMENT
+echo -e "\n\n============================================================编译\n"
 read -p "是否再编译[y/n]?" isCompile
 
 if [ $isCompile == 'y' ]; then
@@ -104,3 +101,4 @@ if [[ $isTestStartup == 'y' ]];then
     cd $targetPath
     sh 23-unpress-startup-test-evn.sh
 fi
+MULTILINECOMMENT
