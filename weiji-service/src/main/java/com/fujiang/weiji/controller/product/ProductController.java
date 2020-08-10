db752fb30922bf14a7e77abdcbd8255357e72d9c
yifan <895115828@qq.com>
2020-08-10 18:23:57 +0800

    yuf

--- weiji-service/src/main/java/com/fujiang/weiji/controller/product/ProductController.java
+++ weiji-service/src/main/java/com/fujiang/weiji/controller/product/ProductController.java
@@ -1,6 +1,8 @@
 package com.fujiang.weiji.controller.product;
 
 import com.fujiang.weiji.entity.product.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/product")
 public class ProductController {
+    private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
     @Value("${server.port}")
     private String port;
 
@@ -23,6 +27,8 @@ public class ProductController {
         //实现
         Product product = new Product();
         //Thread.sleep(3000);
+        logger.info("============================>dddddddwer1111111111111111111111111111111111");
+
         return port + ":" + product.toString();
     }
 }
