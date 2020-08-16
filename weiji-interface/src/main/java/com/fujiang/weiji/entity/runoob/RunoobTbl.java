ab460b31aac33b8f49e590adf002c812f963227c
yifanyifan <895115828@qq.com>
2020-08-16 16:34:05 +0800

    'mybatis'

--- weiji-interface/src/main/java/com/fujiang/weiji/entity/runoob/RunoobTbl.java
+++ weiji-interface/src/main/java/com/fujiang/weiji/entity/runoob/RunoobTbl.java
@@ -1,8 +1,11 @@
 package com.fujiang.weiji.entity.runoob;
 
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Data
 public class RunoobTbl {
     private Integer runoobId;
 
@@ -18,59 +21,4 @@ public class RunoobTbl {
 
     private BigDecimal retert;
 
-    public Integer getRunoobId() {
-        return runoobId;
-    }
-
-    public void setRunoobId(Integer runoobId) {
-        this.runoobId = runoobId;
-    }
-
-    public String getRunoobTitle() {
-        return runoobTitle;
-    }
-
-    public void setRunoobTitle(String runoobTitle) {
-        this.runoobTitle = runoobTitle == null ? null : runoobTitle.trim();
-    }
-
-    public String getRunoobAuthor() {
-        return runoobAuthor;
-    }
-
-    public void setRunoobAuthor(String runoobAuthor) {
-        this.runoobAuthor = runoobAuthor == null ? null : runoobAuthor.trim();
-    }
-
-    public Date getSubmissionDate() {
-        return submissionDate;
-    }
-
-    public void setSubmissionDate(Date submissionDate) {
-        this.submissionDate = submissionDate;
-    }
-
-    public Float getQweqwe() {
-        return qweqwe;
-    }
-
-    public void setQweqwe(Float qweqwe) {
-        this.qweqwe = qweqwe;
-    }
-
-    public Double getSdfsdfsdf() {
-        return sdfsdfsdf;
-    }
-
-    public void setSdfsdfsdf(Double sdfsdfsdf) {
-        this.sdfsdfsdf = sdfsdfsdf;
-    }
-
-    public BigDecimal getRetert() {
-        return retert;
-    }
-
-    public void setRetert(BigDecimal retert) {
-        this.retert = retert;
-    }
 }
