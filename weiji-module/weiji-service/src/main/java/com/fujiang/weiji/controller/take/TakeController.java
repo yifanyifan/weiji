9e78b7463458189b5249d83a2991ca67e531dfb8
Yifan <895115828@qq.com>
2021-01-14 17:34:56 +0800

    'yf'

---
+++ weiji-module/weiji-service/src/main/java/com/fujiang/weiji/controller/take/TakeController.java
@@ -0,0 +1,35 @@
+package com.fujiang.weiji.controller.take;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TakeController {
+    /**
+     * 获取抓取网站集合
+     * 1. 网站名称
+     * 2. 网站抓取列表地址
+     */
+    public void getWebSiteList() {
+        //http://www.coindeskchinese.com/#/bulletin
+    }
+
+    /**
+     * 获取抓取网站规则
+     * 1. 通过列表 获取 网址
+     * 2. 通过网络 抓去 网址内容
+     */
+    public void getWebSiteRule() {
+        //http://www.coindeskchinese.com/#/bulletin
+    }
+
+    /**
+     * 抓取数据
+     * 1. 标题
+     * 2. 日期
+     * 3. 作者
+     * 4. 内容
+     */
+    public void getWebSiteData() {
+
+    }
+}
