e2f7e11e65dd94a2715166dc76aa6a554722d5e5
Yifan <895115828@qq.com>
2021-03-31 16:02:30 +0800

    'yf'

--- weiji-utils/src/main/java/utils/JwtUtil.java
+++ weiji-utils/src/main/java/utils/JwtUtil.java
@@ -1,5 +1,6 @@
 package utils;
 
+import com.alibaba.fastjson.JSON;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.apache.commons.codec.binary.Base64;
@@ -43,14 +44,14 @@ public class JwtUtil {
         Date date = DateUtils.addTime(Calendar.MINUTE, 30);
 
         //todo 优化token的生层规则
-        HashMap<String, Object> claims = new HashMap<>();
-        claims.put(HEADER_USERID, id);
+        HashMap<String, Object> data = new HashMap<>();
+        data.put(HEADER_USERID, id);
 
         String jwt = Jwts.builder()     // 这里其实就是new一个JwtBuilder，设置jwt的body
-                .setClaims(claims)      // 如果有私有声明，一定要先设置这个自己创建的私有的声明，这个是给builder的claim赋值，一旦写在标准的声明赋值之后，就是覆盖了那些标准的声明的
+                //.setClaims(claims)      // 如果有私有声明，一定要先设置这个自己创建的私有的声明，这个是给builder的claim赋值，一旦写在标准的声明赋值之后，就是覆盖了那些标准的声明的
                 //.setIssuedAt(now)     // iat: jwt的签发时间
                 //.setIssuer(issuer)    // issuer：jwt签发人
-                //.setSubject("aaaa")  // sub(Subject)：代表这个JWT的主体，即它的所有人，这个是一个json格式的字符串，可以存放什么userid，roldid之类的，作为什么用户的唯一标志。
+                .setSubject(JSON.toJSONString(data))  // sub(Subject)：代表这个JWT的主体，即它的所有人，这个是一个json格式的字符串，可以存放什么userid，roldid之类的，作为什么用户的唯一标志。
                 .signWith(SignatureAlgorithm.HS512, generalKey())     // 设置签名使用的签名算法和签名使用的秘钥
                 .setExpiration(date)    // 设置签名使用的签名算法和签名使用的秘钥
                 .compact();
