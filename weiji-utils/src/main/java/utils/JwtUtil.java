26719ca5bcbfba21514cf630b8ba5a446834d088
Yifan <895115828@qq.com>
2021-05-10 17:49:27 +0800

    'yf'

--- weiji-utils/src/main/java/utils/JwtUtil.java
+++ weiji-utils/src/main/java/utils/JwtUtil.java
@@ -46,9 +46,10 @@ public class JwtUtil {
         //todo 优化token的生层规则
         HashMap<String, Object> data = new HashMap<>();
         data.put(HEADER_USERID, id);
+        data.put("two", "this is name");
 
         String jwt = Jwts.builder()     // 这里其实就是new一个JwtBuilder，设置jwt的body
-                //.setClaims(claims)      // 如果有私有声明，一定要先设置这个自己创建的私有的声明，这个是给builder的claim赋值，一旦写在标准的声明赋值之后，就是覆盖了那些标准的声明的
+                .setClaims(data)      // 如果有私有声明，一定要先设置这个自己创建的私有的声明，这个是给builder的claim赋值，一旦写在标准的声明赋值之后，就是覆盖了那些标准的声明的
                 //.setIssuedAt(now)     // iat: jwt的签发时间
                 //.setIssuer(issuer)    // issuer：jwt签发人
                 .setSubject(JSON.toJSONString(data))  // sub(Subject)：代表这个JWT的主体，即它的所有人，这个是一个json格式的字符串，可以存放什么userid，roldid之类的，作为什么用户的唯一标志。
