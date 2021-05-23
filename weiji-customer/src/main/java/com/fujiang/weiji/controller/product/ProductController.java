aafe59b9bef39e9b38d2b69247d7d2c375c651c1
yifan <895115828@qq.com>
2021-05-23 11:25:28 +0800

    'yf'

--- weiji-customer/src/main/java/com/fujiang/weiji/controller/product/ProductController.java
+++ weiji-customer/src/main/java/com/fujiang/weiji/controller/product/ProductController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author: Yifan
  * @Description:
@@ -34,6 +36,39 @@ public class ProductController {
     @RequestMapping(value = "insertOrder", method = RequestMethod.GET)
     public String insertOrder() {
         return orderFeign.insert();
+    }
+
+    public static void main(String[] args) {
+        ReentrantLock lock = new ReentrantLock(false);
+        lock.lock();
+        lock.unlock();
+
+        /*ArrayBlockingQueue<String> abq = new ArrayBlockingQueue<String>(2);*/
+
+        /*LinkedBlockingQueue<String> q = new LinkedBlockingQueue<String>(10000);
+        try {
+            q.offer("a");
+            Boolean b = q.offer("hello2",1000,TimeUnit.MILLISECONDS);
+            q.put("sss");
+            q.poll();
+            q.take();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+
+        /*AtomicInteger a = new AtomicInteger(1);
+        int b = a.getAndAdd(2);
+        System.out.println(a);
+        System.out.println(b);
+        int c = a.getAndIncrement();
+        System.out.println(a);
+        System.out.println(c);
+        a.compareAndSet(4, 9);
+        System.out.println(a);*/
+    }
+
+    final int inc(int i) {
+        return (++i == 10) ? 0 : i;
     }
 
 }
